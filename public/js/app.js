9f78c498491ba67363f8e4875507e3933e719aa0
haseena-07 <haseena4677@gmail.com>
2021-11-18 09:46:02 +0000

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+console.log('Client side JS file is loaded')
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#msg1')
+const messageTwo = document.querySelector('#msg2')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Please wait, while we fetch your information'
+    messageTwo.textContent = ''
+
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+            
+        })
+})
+})
+
---
+++ src/app.js
@@ -0,0 +1,102 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const request = require('postman-request');
+const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocode')
+
+const app = express()
+
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname , '../templates/partials')
+
+
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Cube Works'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Us',
+        name: 'Tritian Works'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpmsg: 'You can mail us at tritianworks@gmail.com for any queries',
+        title: 'Help',
+        name: 'Cube Works'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    
+    if(!req.query.address) {
+        return res.send({
+            error: 'Enter an address to know the weather'
+        })
+    }
+
+    geocode(req.query.address, (error,{ latitude, longitude, location} = {} ) => {
+        if(error) {
+            return res.send( {error})
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error) {
+                return res.send({error})
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+
+    })
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search) {
+        return res.send({
+            error: 'Enter a search term'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req,res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Cube Works',
+        errormsg: 'Help article not found'
+    })
+})
+
+app.get('*', (req,res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Cube Works',
+        errormsg: 'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
