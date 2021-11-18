0abdd1c0b42f11bb1104a895e4ed47bb49d00bb1
haseena-07 <haseena4677@gmail.com>
2021-11-18 16:48:05 +0000

    Added about and another forecast about humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude,longitude, callback) => {
         } else if(body.error){
             callback('Unable to find location', undefined)
         }else {
-            callback(undefined, 'Location: ' + body.location.name + ' .Weather description: ' + body.current.weather_descriptions[0] + ' .It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degress out') 
+            callback(undefined, 'Location: ' + body.location.name + ' .Weather description: ' + body.current.weather_descriptions[0] + ' .It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degress out. The humidity is ' + body.current.humidity + "%" ) 
         }
        
     })
